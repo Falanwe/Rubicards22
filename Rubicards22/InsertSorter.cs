240fc92740377549ce6eac3b2ab175730e3c071a
Jean-Noël Gourdol <jngourdol@stormancer.com>
2022-03-28 12:08:51 +0200

    Ajout du tri par insertion

---
+++ Rubicards22/InsertSorter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rubicards22
+{
+    public class InsertSorter : ISorter
+    {
+        public IEnumerable<T> Sort<T>(IEnumerable<T> source) where T : IComparable<T>
+        {
+            var result = new List<T>();
+
+            foreach (T card in source)
+            {
+                for (int i = 0; i <= result.Count; i++)
+                {
+                    if (i == result.Count)
+                    {
+                        result.Add(card);
+                        break;
+                    }
+                    else
+                    {
+                        if (card.CompareTo(result[i]) <= 0)
+                        {
+                            result.Insert(i, card);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- Rubicards22/Program.cs
+++ Rubicards22/Program.cs
@@ -10,10 +10,24 @@ namespace Rubicards22
         {
             var tests = new SorterTests();
 
-            var sorter = new ListSorter();
+            ISorter sorter = new ListSorter();
             if(tests.TestSorter(sorter, out var count))
             {
                 Console.WriteLine($"{nameof(ListSorter)} succeeded in {count} comparisons");
+            }
+            else
+            {
+                Console.WriteLine($"{nameof(ListSorter)} failed");
+            }
+
+            sorter = new InsertSorter();
+            if (tests.TestSorter(sorter, out count))
+            {
+                Console.WriteLine($"{nameof(InsertSorter)} succeeded in {count} comparisons");
+            }
+            else
+            {
+                Console.WriteLine($"{nameof(InsertSorter)} failed");
             }
         }
     }
